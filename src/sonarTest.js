bfff3f77d171ecdffd34fe67ca57cbbbdc146f44
Ubuntu <azureuser@kube01-a.xgnribvq5rjebnok4yzcqflkcd.dx.internal.cloudapp.net>
2026-08-08 02:35:01 +0000

    SonarTest_Fixed

--- src/sonarTest.js
+++ src/sonarTest.js
@@ -1,9 +1,7 @@
-var name = "John Doe"; // Issue: 'var' debe ser cambiada por 'let' or 'const'
-var age = 30; // Issue: 'var' debe ser cambiada por 'let' or 'const'
-var unusedVar = "This is never used"; // Issue: variable sin usar
+const name = "John Doe"; // Issue: 'var' debe ser cambiada por 'let' or 'const'
+const age = 30; // Issue: 'var' debe ser cambiada por 'let' or 'const'
 
 function getUserName() {
-  var name = "Jane Doe"; // Issue: "shadows" la variable 'name'
   return name;
 }
 
@@ -12,22 +10,15 @@ function calculateAge(yearOfBirth) {
   return currentYear - yearOfBirth;
 }
 
-// issue: codigo duplicado
-function calculateUserAge(user) {
-  var currentYear = new Date().getFullYear();
-  return currentYear - user.yearOfBirth;
-}
-
 function unsafeFunction(userInput) {
   // Issue: eval() introduce problemas de seguridad.
-  return eval(userInput);
+  return userInput;
 }
 
 console.log(getUserName());
 console.log(calculateAge(1990));
-console.log(calculateUserAge({ yearOfBirth: 1990 }));
-console.log(unsafeFunction("2 + 2")); // Potencial problema de seguridad
+console.log(safeFunction("2 + 2")); // Potencial problema de seguridad
 
 export default {
-  unsafeFunction,
+  safeFunction,
 };
